7fa3afbb93bec5fd577b93e59a20d6b0f65c4a39
16022877 <16022877@myrp.edu.sg>
2019-05-13 15:20:22 +0800

    hgfghj

--- app/src/main/java/com/example/problemstatement/DBHelperr.java
+++ app/src/main/java/com/example/problemstatement/DBHelperr.java
@@ -1,8 +1,11 @@
 package com.example.problemstatement;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+import android.widget.Toast;
 
 public class DBHelperr extends SQLiteOpenHelper {
     // Start version with 1
@@ -14,7 +17,7 @@ public class DBHelperr extends SQLiteOpenHelper {
     private static final String TABLE_REVISION = "revision";
     private static final String COLUMN_ID = "_id";
     private static final String COLUMN_NAME = "name";
-    private static final String COLUMN_DATE = "date";
+    private static final String COLUMN_STARS = "stars";
 
 
 
@@ -25,11 +28,43 @@ public class DBHelperr extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        String createTableSql = "CREATE TABLE " + TABLE_REVISION +  "("
+                + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + COLUMN_NAME + " TEXT,"
+                + COLUMN_STARS + " REAL )";
+        db.execSQL(createTableSql);
+        Log.i("info" ,"created tables");
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_REVISION);
+        // Create table(s) again
+        onCreate(db);
 
     }
+
+    public void insertRevision(String name, String stars){
+
+        // Get an instance of the database for writing
+        SQLiteDatabase db = this.getWritableDatabase();
+        // We use ContentValues object to store the values for
+        //  the db operation
+        ContentValues values = new ContentValues();
+        // Store the column name as key and the description as value
+        values.put(COLUMN_NAME, name);
+        // Store the column name as key and the date as value
+        values.put(COLUMN_STARS, stars);
+        // Insert the row into the TABLE_TASK
+        db.insert(TABLE_REVISION, null, values);
+        // Close the database connection
+        db.close();
+
+
+
+
+    }
+
 }
--- app/src/main/java/com/example/problemstatement/MainActivity.java
+++ app/src/main/java/com/example/problemstatement/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.problemstatement;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etRev;
+    Button btninsert, btnshow;
+    RadioGroup rg;
+    RadioButton rb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etRev=findViewById(R.id.editText);
+        btninsert=findViewById(R.id.buttonInsert);
+        btnshow=findViewById(R.id.buttonShowList);
+        rg=findViewById(R.id.RadioGroup);
+        int selectedButtonid=rg.getCheckedRadioButtonId();
+        rb=findViewById(selectedButtonid);
+
+
+
+        btninsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DBHelperr db=new DBHelperr(MainActivity.this);
+                //insert revision
+                db.insertRevision(etRev.getText().toString(),rb.getText().toString());
+                db.close();
+                Toast toast=Toast.makeText(getApplicationContext(),"Inserted",Toast.LENGTH_LONG);
+
+                toast.show();
+            }
+        });
+
+
+
+
+
     }
 }
